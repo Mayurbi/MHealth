d4fdd0520290dced55a2cf63ecc101803bb4e270
Mayurbi <bernardespaola12@gmail.com>
2023-11-24 23:55:23 -0300

    Finalizado

--- GlobalSolution/obj/Debug/net8.0/GlobalSolution.AssemblyInfo.cs
+++ GlobalSolution/obj/Debug/net8.0/GlobalSolution.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GlobalSolution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0889ccdda080d9cfb732dc659b48c0556132af07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c6ae4a042976451609d2f1810664e812c85cc75")]
 [assembly: System.Reflection.AssemblyProductAttribute("GlobalSolution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GlobalSolution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
